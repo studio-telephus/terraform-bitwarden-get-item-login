a059eb3a6db51a27c0e044c0e8b28a7b7153f0ba
Kristo Aun <kristoaun@live.com>
2024-01-12 15:59:10 +0200

    item keys

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,8 @@ output "data" {
 
   value = merge(
     {
-      access-key = data.bitwarden_item_login.item.username
-      secret-key = data.bitwarden_item_login.item.password
+      username = data.bitwarden_item_login.item.username
+      password = data.bitwarden_item_login.item.password
     },
 
     merge([
